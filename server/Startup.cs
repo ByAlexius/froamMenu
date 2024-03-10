77d25f909df465b941f3002db2d559ae4648e582
Alexander Rziha <Alexander.rziha@hotmail.com>
2024-03-10 17:34:11 +0100

    first commit

---
+++ server/Startup.cs
@@ -0,0 +1,16 @@
+﻿using AltV.Net.Async;
+
+namespace froamMenu;
+
+public class Startup : AsyncResource
+{
+    public override void OnStart()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnStop()
+    {
+        throw new NotImplementedException();
+    }
+}
